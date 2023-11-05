81d7ea130342c58cadf6ef6dbd9c18e21c380c67
Victor Campos <campos.viictor@gmail.com>
2023-11-05 11:03:09 -0300

    build: init new Barcode Scanner project

---
+++ Barcode Scanner/Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Victor Campos on 05/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
